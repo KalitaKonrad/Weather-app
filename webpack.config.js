347f6286172e468b30ebadbf8264561366712fe6
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-11-23 12:15:14 +0100

    Add MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+ 
 //const packageJSON = require("./package.json");
 
 module.exports = {
@@ -41,12 +43,34 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ],
+      },
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       hash: true,
       template: path.join(__dirname, 'src', 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
     }),
   ]
 }
