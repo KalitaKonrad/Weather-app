996f9a792f4abf7489d70f6b93006bcbdb8e97e9
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-11-23 10:07:00 +0100

    Configure webpack, HtmlWebPackPlugin -> to create HTML file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-//const path = require('path');
+const path = require('path');
 //const packageJSON = require("./package.json");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/app.js",
   output: {
-    filename: "./dist/app.bundle.js"
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js'
   },
   
   module: {
@@ -24,7 +25,7 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       hash: true,
-      filename: "./dist/index.html"
+      template: path.join(__dirname, 'src', 'index.html')
     }),
   ]
 }
